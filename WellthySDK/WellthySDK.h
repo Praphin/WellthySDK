5b0f86cf0d7c0874c02f575741a69e5029c088b8
Praphin SP <psp@wellthy.care>
2022-02-03 12:32:23 +0530

    Initial Commit

---
+++ WellthySDK/WellthySDK.h
@@ -0,0 +1,18 @@
+//
+//  WellthySDK.h
+//  WellthySDK
+//
+//  Created by Praphin SP on 03/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WellthySDK.
+FOUNDATION_EXPORT double WellthySDKVersionNumber;
+
+//! Project version string for WellthySDK.
+FOUNDATION_EXPORT const unsigned char WellthySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WellthySDK/PublicHeader.h>
+
+
